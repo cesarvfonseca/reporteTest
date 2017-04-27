067c8839de9d5d6d976959b7523cd3f8d04c6919
César Valenciano Fonseca <www.cavf.20@gmail.com>
2017-04-27 09:34:44 -0500

    1.0
    Test 1

---
+++ reporteTest/reportvwr/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace reportvwr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVer
+            // 
+            this.btnVer.Location = new System.Drawing.Point(12, 12);
+            this.btnVer.Name = "btnVer";
+            this.btnVer.Size = new System.Drawing.Size(152, 37);
+            this.btnVer.TabIndex = 0;
+            this.btnVer.Text = "Ver";
+            this.btnVer.UseVisualStyleBackColor = true;
+            this.btnVer.Click += new System.EventHandler(this.btnVer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(176, 61);
+            this.Controls.Add(this.btnVer);
+            this.Name = "Form1";
+            this.Text = "Hitomi";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVer;
+    }
+}
+
---
+++ reporteTest/reportvwr/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reportvwr
+{
+    public partial class Form1 : Form
+    {
+
+        int[] largo = {9,10};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string hl(string cadena,int n)
+        {
+            int numberOfLetters = cadena.Length;
+            int ne = n - numberOfLetters;
+            string eBlanco = new string('/', ne);
+            return cadena+eBlanco;
+        }
+
+        private void btnVer_Click(object sender, EventArgs e)
+        {
+            int i=0;
+            string nombre = "Hitomi";
+            int ns = 9;
+            string ape = "Laine";
+            nombre=hl(nombre,ns);
+            foreach (int y in largo)
+            {
+                System.Console.Write("{0} ", y);
+            }
+            
+            //MessageBox.Show("--"+nombre+"--","Important Message");
+        }
+    }
+}
